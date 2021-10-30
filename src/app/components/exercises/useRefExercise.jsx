877a0be8d1dab8b3597c86b4f60098d666c47d99
AlisaWonder <korenskayaalice@gmail.com>
2021-10-30 18:59:11 +0300

    useRef done

--- src/app/components/exercises/useRefExercise.jsx
+++ src/app/components/exercises/useRefExercise.jsx
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useRef } from "react";
 import CollapseWrapper from "../common/collapse";
 const UseRefExercise = () => {
+    const inputRef = useRef();
+
+    const handleClick = () => {
+        inputRef.current.innerHTML = "text";
+    };
+
+    const handleClickWidthAndHeight = () => {
+        inputRef.current.style.width = "80px";
+        inputRef.current.style.height = "150px";
+    };
+
     return (
         <CollapseWrapper title="Упражнение">
             <p className="mt-3">
@@ -11,6 +22,15 @@ const UseRefExercise = () => {
                 <li>Изменится содежимое блока на &quot;text&quot;</li>
                 <li>высота и ширина станут равны 150 и 80 соответственно</li>
             </ul>
+            <button className="btn btn-danger" onClick={handleClick}>
+                Изменить содержимое блока на text
+            </button>
+            <button
+                className="btn btn-secondary"
+                onClick={handleClickWidthAndHeight}
+            >
+                Изменить ширину и высоту
+            </button>
             <div
                 className="bg-primary d-flex flex-row justify-content-center align-items-center rounded"
                 style={{
@@ -18,6 +38,7 @@ const UseRefExercise = () => {
                     width: 60,
                     color: "white"
                 }}
+                ref={inputRef}
             >
                 <small>Блок</small>
             </div>
